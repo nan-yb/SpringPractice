feaa08b040372a04bc84d4edad63611432f22e8a
yb <dydqls5757@gmail.com>
2023-10-09 22:49:24 +0900

    docs : board 테이블 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,14 @@ CREATE TABLE user (
       mod_date datetime DEFAULT NULL,
       PRIMARY KEY (id)
 );
+
+
+CREATE TABLE BOARD(
+    board_id int NOT NULL AUTO_INCREMENT,
+    usrId int NOT NULL
+    board_title varchar(255) DEFAULT NULL,
+    board_content varchar(255) DEFAULT NULL,
+    rog_date datetime DEFAULT NULL,
+    mod_date datetime DEFAULT NULL,
+    PRIMARY KEY (board_id)
+)
