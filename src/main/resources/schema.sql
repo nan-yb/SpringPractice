eb678b2d80c2d3865ee22342cd4e9b4327a0ac9f
yb <dydqls5757@gmail.com>
2023-10-11 21:54:18 +0900

    docs : 댓글 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -17,3 +17,15 @@ CREATE TABLE BOARD(
     mod_date datetime DEFAULT NULL,
     PRIMARY KEY (board_id)
 )
+
+
+CREATE TABLE COMMENT(
+      comment_id int NOT NULL AUTO_INCREMENT,
+      board_id int NOT NULL ,
+      usrId int NOT NULL   ,
+      comment_content varchar(255) DEFAULT NULL,
+      rog_date datetime DEFAULT NULL,
+      mod_date datetime DEFAULT NULL,
+      PRIMARY KEY (comment_id)
+)
+
