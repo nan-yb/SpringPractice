334e0c21edb95855abd528977d6793b3ed6d48c2
yongbin_home <dydqls5757@gmail.com>
2023-12-24 17:28:14 +0900

    feat :: msa서비스 간의 통신 방법

--- spring-msa/item-service/src/main/java/org/msa/item/service/ItemService.java
+++ spring-msa/item-service/src/main/java/org/msa/item/service/ItemService.java
@@ -2,18 +2,26 @@ package org.msa.item.service;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
 import org.msa.item.domain.Item;
 import org.msa.item.dto.ItemDTO;
+import org.msa.item.feign.HistoryFeignClient;
 import org.msa.item.repository.ItemRepository;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ItemService {
 	private final ItemRepository itemRepository;
+	private final HistoryFeignClient historyFeignClient;
+	private final RestTemplate restTemplate;
 	
 	public void insertItem(ItemDTO itemDTO , String accountId) {
 		SimpleDateFormat form = new SimpleDateFormat("yyyyMMddHHmmss");
@@ -30,6 +38,16 @@ public class ItemService {
 				.updDts(date)
 				.build();
 		itemRepository.save(item);
+
+		HashMap<String , Object> historyMap = new HashMap<>();
+		historyMap.put("accountId" , accountId);
+		historyMap.put("itemId" , itemDTO.getId());
+
+//		FeignClient 이용
+//		log.info("feign result = {}" , historyFeignClient.saveHistory(historyMap));
+
+//		restTemplate 이용
+		log.info("resttemplate result = {}" , restTemplate.postForObject("http://HISTORY_SERVICE/v1/history/save" , historyMap , String.class));
 	}
 
 }
